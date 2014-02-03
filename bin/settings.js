06169abd9f25a19d8a0a2a4c9ede3c88d0ad88ca
Yiwei Chen <yiwei.chen@rackspace.com>
2014-02-02 23:06:17 -0800

    opps forgot to add new file

---
+++ bin/settings.js
@@ -0,0 +1,18 @@
+
+/** 
+ * USERS array
+ */
+var users = [
+  'ychen11'
+];
+
+/**
+ * Setting module
+ */
+exports.settings = {
+  NICK_NAME: 'yay_bot',
+  PORT: 6666,
+  IRC_SERVER: 'irc.freenode.net',
+  CHANNELS: ['#test-yiwei'],
+  USERS: users
+};
